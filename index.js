5fe0c760c7243571072fd276ee2fa3c08762fa39
bella7898 <77554409+bella7898@users.noreply.github.com>
2022-12-27 01:59:39 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'New Year, New Me'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("title").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
