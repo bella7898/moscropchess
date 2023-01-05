64c519a810ae753cf261ce6767221efa9a38d3bb
bella7898 <77554409+bella7898@users.noreply.github.com>
2023-01-04 16:08:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'New Year, New Me'; /* The text */
+var txt = '#New Year, New Me'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
